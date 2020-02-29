70ce3cfcda059480b12ff1be271bf80e791f4d8f
Cynthia Caroline <39481606+pocaroline34@users.noreply.github.com>
2020-02-29 11:03:09 +0700

    Add files via upload
    first fix

---
+++ augmented.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 28 09:21:23 2020
+
+@author: Sutanmudo
+"""
+# Importing necessary functions 
+from keras.preprocessing.image import ImageDataGenerator,img_to_array,load_img 
+import glob  
+# Initialising the ImageDataGenerator class. 
+# We will pass in the augmentation parameters in the constructor. 
+
+datagen = ImageDataGenerator( 
+        rotation_range = 40, 
+        shear_range = 0.2, 
+        zoom_range = 0.2, 
+        horizontal_flip = True, 
+        brightness_range = (0.5, 1.5)) 
+
+l = 0
+file = glob.glob("fcv/*.jpg")
+for files in range(len(file)):
+    # Loading a sample image  
+    img = load_img(file[l])
+    # Converting the input sample image to an array 
+    x = img_to_array(img)
+    # Reshaping the input image 
+    x = x.reshape((1, ) + x.shape)
+    # Generating and saving 5 augmented samples  
+    # using the above defined parameters.  
+    i = 0
+    for batch in datagen.flow(x, batch_size = 1,
+                              save_to_dir ='result',
+                              save_prefix ='image',
+                              save_format ='jpg'):
+        i += 1
+        if i > 5: 
+            break
+    l += 1
